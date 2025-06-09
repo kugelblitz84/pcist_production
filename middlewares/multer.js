80acc98cb513f6d0c7ded38cb1733d90f19bb61b
kugelblitz84 <111196826+kugelblitz84@users.noreply.github.com>
2025-06-09 18:09:11 +0600

    added the template codes to use later.

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,14 +1,10 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../configs/cloudinary.js';
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'event_images',
-    allowed_formats: ['jpg', 'png', 'jpeg'],
-  },
-});
+const storage = multer.diskStorage({
+  filename: function(req, file, callback){
+    callback(null, file.originalname)
+  }
+})
 
 const parser = multer({ storage });
 
