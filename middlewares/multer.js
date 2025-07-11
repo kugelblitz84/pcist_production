e308404e093d7e714eb1423fabd68a5582232f99
kugelblitz84 <sadimahmud225@gmail.com>
2025-07-11 15:31:22 +0600

    finsihed fcm and event uploads

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -11,4 +11,11 @@ const storage = multer.diskStorage({
 
 const parser = multer({ storage });
 
-export default parser;
+const uploadEventImages = parser.fields([
+  { name: 'image1', maxCount: 1 },
+  { name: 'image2', maxCount: 1 },
+  { name: 'image3', maxCount: 1 },
+  { name: 'image4', maxCount: 1 },
+]);
+
+export default uploadEventImages;
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import parser from '../middlewares/multer.js';
+import uploadEventImages from '../middlewares/multer.js';
 import {addEvent, getEvents, GetOneEventUsingId, updateEvent, deleteEvent} from '../controllers/eventController.js'
 import adminAuth from '../middlewares/adminAuth.js';
 
 const eventRoutes = express.Router()
 
-eventRoutes.post('/add_event', parser.single('image'), adminAuth, addEvent)
+eventRoutes.post('/add_event', uploadEventImages, adminAuth, addEvent)
 eventRoutes.get('/get_all_event', getEvents)
 eventRoutes.get('/get_one_event/:id', GetOneEventUsingId)
 eventRoutes.put('/update_event/:id', adminAuth, updateEvent)
