80754e89f9ec604beba01228e37b497f60ed7c39
kugelblitz84 <sadimahmud225@gmail.com>
2025-08-08 22:32:43 +0600

    checking github updates

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/v1/event", eventRoutes);
 app.use("/api/v1/notification", NotificationRouter);
 
 app.get("/", (req, res) => {
-  res.send("API Working");
+  res.send("API update Working");
 });
 
 //app.listen(port, () => console.log("Server started on PORT : " + port));
