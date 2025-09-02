30092d18dc294bac5bed5e38e768e226b912bf32
kugelblitz84 <sadimahmud225@gmail.com>
2025-09-02 23:30:15 +0600

    .

--- routes/notificationsRoutes.js
+++ routes/notificationsRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import { notifyAllUsers, notifyOneUser, sendPadStatementEmail, sendInvoiceEmail } from '../controllers/notificationController.js';
+import { notifyAllUsers, notifyOneUser} from '../controllers/notificationController.js';
 import adminAuth from '../middlewares/adminAuth.js';
 
 const NotificationRouter = express.Router()
 
 NotificationRouter.post('/notify_all_users', adminAuth, notifyAllUsers);
 NotificationRouter.post('/notify_one/:token', adminAuth,  notifyOneUser);
-NotificationRouter.post('/send_pad_statement', adminAuth, sendPadStatementEmail);
-NotificationRouter.post('/send_invoice', adminAuth, sendInvoiceEmail);
+
 
 export default NotificationRouter;
